5ad0a4862e66b4b4c3f8f13f3aac0c13c9719ef5
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2023-05-05 17:26:19 -0400

    update

--- tokens/tailwindTokens/tailwindFontFamilies.js
+++ tokens/tailwindTokens/tailwindFontFamilies.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 const tailwindFontFamily = Object.entries(defaultTheme.fontFamily);
+const tailwindBorderWidth = Object.entries(defaultTheme.borderWidth);
 
 const fontFamilyObj = tailwindFontFamily.reduce((acc, [key, value], index) => {
   acc[key] = {
@@ -10,11 +11,25 @@ const fontFamilyObj = tailwindFontFamily.reduce((acc, [key, value], index) => {
   return acc;
 }, {});
 
+const borderWidthObj = tailwindBorderWidth.reduce(
+  (acc, [key, value], index) => {
+    acc[key] = {
+      value: value,
+    };
+    return acc;
+  },
+  {}
+);
+
 module.exports = {
   tailwind: {
     fontFamily: {
       type: 'fontFamilies',
       ...fontFamilyObj,
+    },
+    border: {
+      type: 'borderWidth',
+      ...borderWidthObj,
     },
   },
 };
