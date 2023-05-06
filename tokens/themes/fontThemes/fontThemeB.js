39ad34019c352d3a7463fb608a839072cc74f6f9
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2023-05-06 06:58:34 -0400

    add fonts

---
+++ tokens/themes/fontThemes/fontThemeB.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'fontTheme/themeB': {
+    fontFamily: {
+      type: 'fontFamilies',
+      primary: { value: 'Faustina' },
+      secondary: { value: 'IBM Plex Mono' },
+    },
+  },
+};
