170d78281b32a67c33dbcc621319d5091b6a1acc
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2023-05-06 06:48:49 -0400

    add semantic colors

--- tokens/themes/colorThemes/colorThemeA.js
+++ tokens/themes/colorThemes/colorThemeA.js
@@ -6,6 +6,11 @@ module.exports = {
       primary: colors.blue,
       secondary: colors.pink,
       neutral: colors.neutral,
+      semantic: {
+        success: colors.green[500],
+        warning: colors.yellow[500],
+        danger: colors.red[500],
+      },
     },
   },
 };
--- tokens/themes/colorThemes/colorThemeB.js
+++ tokens/themes/colorThemes/colorThemeB.js
@@ -5,6 +5,12 @@ module.exports = {
     color: {
       primary: colors.green,
       secondary: colors.yellow,
+      neutral: colors.neutral,
+      semantic: {
+        success: colors.green[500],
+        warning: colors.yellow[500],
+        danger: colors.red[500],
+      },
     },
   },
 };
